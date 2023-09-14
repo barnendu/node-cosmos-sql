4ce05a07682afb6b53d73dcb87d1103175d32253
barnendu <barnendu.pal001@gmail.com>
2023-09-15 00:45:01 +0100

    first commit

---
+++ config.js
@@ -0,0 +1,11 @@
+// @ts-check
+
+const config = {
+    endpoint: "<Your Azure Cosmos account URI>",
+    key: "<Your Azure Cosmos account key>",
+    databaseId: "Tasks",
+    containerId: "Items",
+    partitionKey: { kind: "Hash", paths: ["/category"] }
+  };
+  
+  module.exports = config;
